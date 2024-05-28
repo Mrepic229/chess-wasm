396b5df0d297e9fb01d6caa4a12e0f6dfa53a4ed
Mrepic229 <jis69508@gmail.com>
2024-05-27 20:10:02 -0500

    handeled invalid notation, switch boards, whose turn indicator

--- src/index.js
+++ src/index.js
@@ -1,24 +1,93 @@
 import init, { next_move } from "../pkg/chess_web_app.js";
+
 var board_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-var link_to_image = "https://mutsuntsai.github.io/fen-tool/gen/?fen="
+var link_to_image = "https://mutsuntsai.github.io/fen-tool/gen/?fen=";
+var alt_link_to_image = "http://www.fen-to-image.com/image/64/single/coords/";
+var is_white_to_move = true;
+var moveId = document.getElementById("chess_notation");
 
 function do_move() {
-    const moveId = document.getElementById("chess_notation");
+    let new_board_fen = next_move(String(moveId.value), board_fen);
+
+    if (new_board_fen == "cannot parse FEN") {
+        fen_error();
+        return;
+    }
+    if (new_board_fen == "cannot parse san") {
+        san_error();
+        return;
+    }
+    if (new_board_fen == "cannot simulate move") {
+        wft_error();
+        return;
+    }
+
+    switch_whose_turn();
+    display(new_board_fen);
+}
+
+function display(new_board_fen) {
     const boardImageId = document.getElementById("board_image");
 
-    let new_board = next_move(String(moveId.value), board_fen);
+    board_fen = new_board_fen;
 
-    board_fen = new_board
-
+    console.log(link_to_image);
     boardImageId.src = link_to_image.concat(board_fen);
-    
-    console.log(new_board);
+}
+
+function fen_error() {
+    console.log("FEN error");
+    report_error_to_user("This error should never happen");
+}
+
+function san_error() {
+    console.log("SAN error");
+    report_error_to_user("Enter valid algebraic chess notation");
+}
+
+function wft_error() {
+    console.log("wtf error");
+    report_error_to_user("This error should never happen");
+}
+
+function report_error_to_user(my_string) {
+    const errorReportId = document.getElementById("error_report");
+    errorReportId.textContent = my_string;
+
+    setTimeout(function() {errorReportId.textContent = ""},
+        2500
+    );
+}
+
+function switch_boards() {
+    let temp = link_to_image;
+    link_to_image = alt_link_to_image;
+    alt_link_to_image = temp;
+
+    display(board_fen);
+}
+
+function switch_whose_turn() {
+    const whosMoveId = document.getElementById("whos_move");
+    if (is_white_to_move) {
+        is_white_to_move = false;
+        whosMoveId.textContent = "Black";
+    } else {
+        is_white_to_move = true;
+        whosMoveId.textContent = "White";
+    }
 }
 
 init().then(() => {
+    moveId.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("go_button").click();
+        }
+    });
 
-    console.log(board_fen);
+    display(board_fen);
+
+    window.switch_boards = switch_boards;
     window.do_move = do_move;
 });
-
-
